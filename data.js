5ce9704458a77ec9a3e696a1986bc483efb0b4e3
MiscExpFFL <tomtom2023@yahoo.com>
2026-09-01 00:10:47 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,955 @@
+window.MAKERS_DATA = {
+  "seasons": {
+    "2023": {
+      "champion": "The Vulvarines",
+      "champManager": "Jim",
+      "runnerUp": "The Dink Wads",
+      "runnerManager": "Nick",
+      "third": "The Mustache riders",
+      "thirdManager": "TomD",
+      "standings": [
+        [
+          "The Vulvarines",
+          "Jim",
+          "10-4",
+          "1404.87",
+          "1333.76",
+          66
+        ],
+        [
+          "The Dink Wads",
+          "Nick",
+          "10-4",
+          "1533.23",
+          "1320.29",
+          64
+        ],
+        [
+          "The Mustache riders",
+          "TomD",
+          "7-7",
+          "1620.29",
+          "1362.56",
+          28
+        ],
+        [
+          "Period bloods",
+          "Nate",
+          "9-5",
+          "1485.99",
+          "1346.05",
+          24
+        ],
+        [
+          "Pump and Go",
+          "Tommy",
+          "8-6",
+          "1430.03",
+          "1403.11",
+          20
+        ],
+        [
+          "In Brock We Trust",
+          "El Rubio",
+          "7-7",
+          "1442.41",
+          "1543.11",
+          17
+        ],
+        [
+          "Criterus",
+          "Chris",
+          "5-9",
+          "1234.57",
+          "1406.32",
+          20
+        ],
+        [
+          "The A Gap",
+          "Adam",
+          "4-10",
+          "1177.17",
+          "1415.92",
+          30
+        ],
+        [
+          "Eviscerators",
+          "Andrew",
+          "4-10",
+          "1299.03",
+          "1457.19",
+          24
+        ],
+        [
+          "Predacious Fungi",
+          "Max",
+          "6-8",
+          "1281.33",
+          "1320.61",
+          81
+        ]
+      ],
+      "playoffs": [
+        [
+          "Quarterfinal",
+          "The Mustache riders",
+          "108.89",
+          "Pump and Go",
+          "68.54"
+        ],
+        [
+          "Quarterfinal",
+          "Period bloods",
+          "100.01",
+          "In Brock We Trust",
+          "61.61"
+        ],
+        [
+          "Semifinal",
+          "The Dink Wads",
+          "114.95",
+          "The Mustache riders",
+          "80.20"
+        ],
+        [
+          "Semifinal",
+          "The Vulvarines",
+          "108.64",
+          "Period bloods",
+          "80.79"
+        ],
+        [
+          "Final",
+          "The Vulvarines",
+          "107.59",
+          "The Dink Wads",
+          "84.90"
+        ],
+        [
+          "3rd Place",
+          "The Mustache riders",
+          "111.97",
+          "Period bloods",
+          "94.63"
+        ],
+        [
+          "5th Place",
+          "Pump and Go",
+          "115.46",
+          "In Brock We Trust",
+          "76.53"
+        ],
+        [
+          "7th Place",
+          "Criterus",
+          "87.59",
+          "The A Gap",
+          "61.14"
+        ],
+        [
+          "9th Place",
+          "Eviscerators",
+          "123.37",
+          "Predacious Fungi",
+          "119.11"
+        ]
+      ],
+      "keepers": [
+        [
+          "Jim",
+          "The Vulvarines",
+          "Amon-Ra St. Brown",
+          "R4"
+        ],
+        [
+          "Tommy",
+          "Pump and Go",
+          "Brandon Aiyuk",
+          "R6"
+        ],
+        [
+          "Tommy",
+          "Pump and Go",
+          "Garrett Wilson",
+          "R13"
+        ],
+        [
+          "Nate",
+          "Period bloods",
+          "Josh Jacobs",
+          "R5"
+        ],
+        [
+          "Max",
+          "Predacious Fungi",
+          "Rhamondre Stevenson",
+          "R6"
+        ],
+        [
+          "TomD",
+          "The Mustache riders",
+          "Jalen Hurts",
+          "R6"
+        ],
+        [
+          "Andrew",
+          "Eviscerators",
+          "George Pickens",
+          "R10"
+        ]
+      ],
+      "note": "Jim won the title despite Nick leading the regular-season points race and TomD posting the season\u2019s highest points-for total. Max somehow paired 81 moves with a 6-8 record, establishing an early league tradition: activity and wisdom are separate statistics."
+    },
+    "2024": {
+      "champion": "Pump and Go",
+      "champManager": "Tommy",
+      "runnerUp": "Double Drafters",
+      "runnerManager": "Nate",
+      "third": "Predacious Fungi",
+      "thirdManager": "Max",
+      "standings": [
+        [
+          "Pump and Go",
+          "Tommy",
+          "12-2",
+          "1603.16",
+          "1344.92",
+          13
+        ],
+        [
+          "Double Drafters",
+          "Nate",
+          "9-5",
+          "1556.80",
+          "1476.02",
+          13
+        ],
+        [
+          "Predacious Fungi",
+          "Max",
+          "8-6",
+          "1648.80",
+          "1478.16",
+          64
+        ],
+        [
+          "Massive TDs",
+          "Nick",
+          "8-6",
+          "1430.10",
+          "1358.50",
+          53
+        ],
+        [
+          "Hot Green Rain",
+          "Andrew",
+          "8-6",
+          "1629.44",
+          "1455.50",
+          31
+        ],
+        [
+          "The Mustache riders",
+          "TomD",
+          "6-8",
+          "1453.98",
+          "1622.50",
+          18
+        ],
+        [
+          "Criterus",
+          "Chris",
+          "3-11",
+          "1269.54",
+          "1560.42",
+          17
+        ],
+        [
+          "In Brock We Trust",
+          "El Rubio",
+          "5-9",
+          "1379.34",
+          "1485.48",
+          9
+        ],
+        [
+          "The Silverback Maulers",
+          "Jim",
+          "6-8",
+          "1390.34",
+          "1491.62",
+          82
+        ],
+        [
+          "The A Gap",
+          "Adam",
+          "5-9",
+          "1432.40",
+          "1520.78",
+          20
+        ]
+      ],
+      "playoffs": [
+        [
+          "Quarterfinal",
+          "Massive TDs",
+          "125.68",
+          "Hot Green Rain",
+          "117.46"
+        ],
+        [
+          "Quarterfinal",
+          "Predacious Fungi",
+          "153.74",
+          "The Mustache riders",
+          "88.44"
+        ],
+        [
+          "Semifinal",
+          "Pump and Go",
+          "137.28",
+          "Massive TDs",
+          "125.60"
+        ],
+        [
+          "Semifinal",
+          "Double Drafters",
+          "154.34",
+          "Predacious Fungi",
+          "125.58"
+        ],
+        [
+          "Final",
+          "Pump and Go",
+          "140.62",
+          "Double Drafters",
+          "131.28"
+        ],
+        [
+          "3rd Place",
+          "Predacious Fungi",
+          "113.88",
+          "Massive TDs",
+          "87.44"
+        ],
+        [
+          "5th Place",
+          "Hot Green Rain",
+          "143.24",
+          "The Mustache riders",
+          "88.98"
+        ],
+        [
+          "7th Place",
+          "Criterus",
+          "137.26",
+          "In Brock We Trust",
+          "107.30"
+        ],
+        [
+          "9th Place",
+          "The Silverback Maulers",
+          "117.10",
+          "The A Gap",
+          "89.58"
+        ]
+      ],
+      "keepers": [
+        [
+          "Jim",
+          "The Silverback Maulers",
+          "Amon-Ra St. Brown",
+          "R2"
+        ],
+        [
+          "Nick",
+          "Massive TDs",
+          "Alvin Kamara",
+          "R5"
+        ],
+        [
+          "Nick",
+          "Massive TDs",
+          "Mike Evans",
+          "R7"
+        ],
+        [
+          "TomD",
+          "The Mustache riders",
+          "Raheem Mostert",
+          "R10"
+        ],
+        [
+          "Andrew",
+          "Hot Green Rain",
+          "George Pickens",
+          "R8"
+        ],
+        [
+          "Tommy",
+          "Pump and Go",
+          "Garrett Wilson",
+          "R11"
+        ]
+      ],
+      "note": "Tommy went 12-2 and finished the job. Max actually led the league in points, Andrew was only 19.36 points behind him, and Jim made 82 moves \u2014 the current single-season transaction-churn record."
+    },
+    "2025": {
+      "champion": "The Eviscerators",
+      "champManager": "Andrew",
+      "runnerUp": "The A Gap",
+      "runnerManager": "Adam",
+      "third": "The Moose Knuckles",
+      "thirdManager": "Jim",
+      "standings": [
+        [
+          "The Eviscerators",
+          "Andrew",
+          "9-5",
+          "1531.16",
+          "1334.22",
+          35
+        ],
+        [
+          "The A Gap",
+          "Adam",
+          "8-6",
+          "1490.08",
+          "1400.04",
+          30
+        ],
+        [
+          "The Moose Knuckles",
+          "Jim",
+          "7-7",
+          "1585.46",
+          "1474.14",
+          65
+        ],
+        [
+          "Predacious Fungi",
+          "Max",
+          "8-6",
+          "1540.34",
+          "1388.14",
+          52
+        ],
+        [
+          "The Mustache riders",
+          "TomD",
+          "8-6",
+          "1481.88",
+          "1420.64",
+          22
+        ],
+        [
+          "TDs In Your Face",
+          "Nick",
+          "7-7",
+          "1501.56",
+          "1475.38",
+          66
+        ],
+        [
+          "Pump and Go",
+          "Tommy",
+          "7-7",
+          "1468.24",
+          "1556.62",
+          17
+        ],
+        [
+          "Criterus",
+          "Chris",
+          "5-9",
+          "1360.82",
+          "1490.42",
+          18
+        ],
+        [
+          "Kareem all over your Hunt",
+          "Billy",
+          "6-8",
+          "1281.38",
+          "1459.68",
+          14
+        ],
+        [
+          "Revenge of the period bloods",
+          "Nate",
+          "5-9",
+          "1284.16",
+          "1525.80",
+          15
+        ]
+      ],
+      "playoffs": [
+        [
+          "Quarterfinal",
+          "The Moose Knuckles",
+          "126.10",
+          "The Mustache riders",
+          "82.16"
+        ],
+        [
+          "Quarterfinal",
+          "The A Gap",
+          "155.30",
+          "TDs In Your Face",
+          "134.42"
+        ],
+        [
+          "Semifinal",
+          "The Eviscerators",
+          "124.92",
+          "The Moose Knuckles",
+          "120.50"
+        ],
+        [
+          "Semifinal",
+          "The A Gap",
+          "147.00",
+          "Predacious Fungi",
+          "120.40"
+        ],
+        [
+          "Final",
+          "The Eviscerators",
+          "122.60",
+          "The A Gap",
+          "87.78"
+        ],
+        [
+          "3rd Place",
+          "The Moose Knuckles",
+          "116.60",
+          "Predacious Fungi",
+          "91.24"
+        ],
+        [
+          "5th Place",
+          "The Mustache riders",
+          "138.36",
+          "TDs In Your Face",
+          "96.70"
+        ],
+        [
+          "7th Place",
+          "Pump and Go",
+          "139.20",
+          "Criterus",
+          "59.26"
+        ],
+        [
+          "9th Place",
+          "Kareem all over your Hunt",
+          "70.32",
+          "Revenge of the period bloods",
+          "54.14"
+        ]
+      ],
+      "keepers": [
+        [
+          "Nick",
+          "TDs In Your Face",
+          "Jameson Williams",
+          "R9"
+        ],
+        [
+          "Chris",
+          "Criterus",
+          "T.J. Hockenson",
+          "R12"
+        ],
+        [
+          "TomD",
+          "The Mustache riders",
+          "Brock Bowers",
+          "R10"
+        ],
+        [
+          "Adam",
+          "The A Gap",
+          "Jayden Daniels",
+          "R8"
+        ],
+        [
+          "Tommy",
+          "Pump and Go",
+          "Brian Thomas Jr.",
+          "R8"
+        ]
+      ],
+      "note": "Andrew won the title after a 9-5 regular season, beating Jim by 4.42 in the semifinal and Adam by 34.82 in the final. Jim led the league in points but finished third. Billy entered as a new manager in 2025 and starts his own history \u2014 he does not inherit El Rubio\u2019s record."
+    }
+  },
+  "currentTeams": [
+    [
+      "The Eviscerators",
+      "Andrew",
+      "A+",
+      "13-1",
+      1781.85
+    ],
+    [
+      "The Moose Knuckles",
+      "Jim",
+      "A+",
+      "12-2",
+      1747.79
+    ],
+    [
+      "TDs In Your Face",
+      "Nick",
+      "B+",
+      "8-6",
+      1715.4
+    ],
+    [
+      "The Mustache riders",
+      "TomD",
+      "B",
+      "9-5",
+      1714.05
+    ],
+    [
+      "Criterus",
+      "Chris",
+      "C+",
+      "7-7",
+      1675.2
+    ],
+    [
+      "Kareem all over your Hunt",
+      "Billy",
+      "C+",
+      "8-6",
+      1660.1
+    ],
+    [
+      "Pump and Go",
+      "Tommy",
+      "D+",
+      "6-8",
+      1621.0
+    ],
+    [
+      "The A Gap",
+      "Adam",
+      "D",
+      "3-11",
+      1595.96
+    ],
+    [
+      "Predacious Fungi",
+      "Max",
+      "D-",
+      "3-11",
+      1570.24
+    ],
+    [
+      "Revenge of the period bloods",
+      "Nate",
+      "F",
+      "1-13",
+      1568.44
+    ]
+  ],
+  "keepers2026": [
+    [
+      "Jim",
+      "The Moose Knuckles",
+      "Jaxon Smith-Njigba",
+      "R3"
+    ],
+    [
+      "Andrew",
+      "The Eviscerators",
+      "George Pickens",
+      "R5"
+    ],
+    [
+      "Tommy",
+      "Pump and Go",
+      "DeVonta Smith",
+      "R5"
+    ],
+    [
+      "Max",
+      "Predacious Fungi",
+      "Emeka Egbuka",
+      "R6"
+    ],
+    [
+      "Nick",
+      "TDs In Your Face",
+      "Jameson Williams",
+      "R7"
+    ],
+    [
+      "Andrew",
+      "The Eviscerators",
+      "Chris Olave",
+      "R7"
+    ],
+    [
+      "TomD",
+      "The Mustache riders",
+      "Brock Bowers",
+      "R8"
+    ],
+    [
+      "Chris",
+      "Criterus",
+      "Cam Skattebo",
+      "R9"
+    ],
+    [
+      "Tommy",
+      "Pump and Go",
+      "Tyler Warren",
+      "R9"
+    ],
+    [
+      "Max",
+      "Predacious Fungi",
+      "Tucker Kraft",
+      "R10"
+    ]
+  ],
+  "franchises": [
+    {
+      "manager": "Chris",
+      "current": "Criterus",
+      "since": 2023,
+      "aliases": [
+        "2022: Always Optimal",
+        "2023\u201326: Criterus"
+      ]
+    },
+    {
+      "manager": "Andrew",
+      "current": "The Eviscerators",
+      "since": 2023,
+      "aliases": [
+        "2022: Eviscerators",
+        "2023: Eviscerators",
+        "2024: Hot Green Rain",
+        "2025\u201326: The Eviscerators"
+      ]
+    },
+    {
+      "manager": "Nate",
+      "current": "Revenge of the period bloods",
+      "since": 2023,
+      "aliases": [
+        "2022: Ninth Place NATES",
+        "2023: Period bloods",
+        "2024: Double Drafters",
+        "2025\u201326: Revenge of the period bloods"
+      ]
+    },
+    {
+      "manager": "Max",
+      "current": "Predacious Fungi",
+      "since": 2023,
+      "aliases": [
+        "2022\u201326: Predacious Fungi"
+      ]
+    },
+    {
+      "manager": "Tommy",
+      "current": "Pump and Go",
+      "since": 2023,
+      "aliases": [
+        "2022: Deshaun\u2019s Terrible Towel",
+        "2023\u201326: Pump and Go"
+      ]
+    },
+    {
+      "manager": "Adam",
+      "current": "The A Gap",
+      "since": 2023,
+      "aliases": [
+        "2022\u201326: The A Gap"
+      ]
+    },
+    {
+      "manager": "Nick",
+      "current": "TDs In Your Face",
+      "since": 2023,
+      "aliases": [
+        "2022: TDs IN YO FACE",
+        "2023: The Dink Wads",
+        "2024: Massive TDs",
+        "2025\u201326: TDs In Your Face"
+      ]
+    },
+    {
+      "manager": "TomD",
+      "current": "The Mustache riders",
+      "since": 2023,
+      "aliases": [
+        "2022\u201326: The Mustache riders"
+      ]
+    },
+    {
+      "manager": "Jim",
+      "current": "The Moose Knuckles",
+      "since": 2023,
+      "aliases": [
+        "2022: Raw Doggin\u2019 Randos",
+        "2023: The Vulvarines",
+        "2024: The Silverback Maulers",
+        "2025\u201326: The Moose Knuckles"
+      ]
+    },
+    {
+      "manager": "Billy",
+      "current": "Kareem all over your Hunt",
+      "since": 2025,
+      "aliases": [
+        "2025\u201326: Kareem all over your Hunt"
+      ],
+      "note": "Billy replaced El Rubio in 2025. His franchise history begins in 2025 and does not inherit El Rubio\u2019s 2023\u201324 record."
+    },
+    {
+      "manager": "El Rubio",
+      "current": "Retired / former manager",
+      "since": 2023,
+      "aliases": [
+        "2022: Bad Newz Kennels",
+        "2023\u201324: In Brock We Trust"
+      ],
+      "note": "El Rubio\u2019s history stops after 2024. It remains separate from Billy\u2019s record."
+    }
+  ],
+  "records": [
+    {
+      "manager": "Tommy",
+      "w": 27,
+      "l": 15,
+      "pf": 4501.43,
+      "seasons": 3,
+      "titles": 1,
+      "best": 1,
+      "pct": 0.6428571428571429
+    },
+    {
+      "manager": "Nick",
+      "w": 25,
+      "l": 17,
+      "pf": 4464.889999999999,
+      "seasons": 3,
+      "titles": 0,
+      "best": 2,
+      "pct": 0.5952380952380952
+    },
+    {
+      "manager": "Jim",
+      "w": 23,
+      "l": 19,
+      "pf": 4380.67,
+      "seasons": 3,
+      "titles": 1,
+      "best": 1,
+      "pct": 0.5476190476190477
+    },
+    {
+      "manager": "Nate",
+      "w": 23,
+      "l": 19,
+      "pf": 4326.95,
+      "seasons": 3,
+      "titles": 0,
+      "best": 2,
+      "pct": 0.5476190476190477
+    },
+    {
+      "manager": "Max",
+      "w": 22,
+      "l": 20,
+      "pf": 4470.47,
+      "seasons": 3,
+      "titles": 0,
+      "best": 3,
+      "pct": 0.5238095238095238
+    },
+    {
+      "manager": "TomD",
+      "w": 21,
+      "l": 21,
+      "pf": 4556.15,
+      "seasons": 3,
+      "titles": 0,
+      "best": 3,
+      "pct": 0.5
+    },
+    {
+      "manager": "Andrew",
+      "w": 21,
+      "l": 21,
+      "pf": 4459.63,
+      "seasons": 3,
+      "titles": 1,
+      "best": 1,
+      "pct": 0.5
+    },
+    {
+      "manager": "Adam",
+      "w": 17,
+      "l": 25,
+      "pf": 4099.65,
+      "seasons": 3,
+      "titles": 0,
+      "best": 2,
+      "pct": 0.40476190476190477
+    },
+    {
+      "manager": "Chris",
+      "w": 13,
+      "l": 29,
+      "pf": 3864.9299999999994,
+      "seasons": 3,
+      "titles": 0,
+      "best": 99,
+      "pct": 0.30952380952380953
+    },
+    {
+      "manager": "El Rubio",
+      "w": 12,
+      "l": 16,
+      "pf": 2821.75,
+      "seasons": 2,
+      "titles": 0,
+      "best": 99,
+      "pct": 0.42857142857142855
+    },
+    {
+      "manager": "Billy",
+      "w": 6,
+      "l": 8,
+      "pf": 1281.38,
+      "seasons": 1,
+      "titles": 0,
+      "best": 99,
+      "pct": 0.42857142857142855
+    }
+  ],
+  "trades2025": [
+    [
+      "Nov. 13",
+      "Billy / Kareem all over your Hunt",
+      "Alvin Kamara",
+      "Andrew / The Eviscerators",
+      "Chris Olave"
+    ],
+    [
+      "Nov. 11",
+      "Jim / The Moose Knuckles",
+      "Tyler Allgeier",
+      "TomD / The Mustache riders",
+      "Kamari Vidal"
+    ]
+  ]
+};
