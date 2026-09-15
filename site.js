46b5bfb21581c5444233ef1d3d9297129fb51605
MiscExpFFL <tomtom2023@yahoo.com>
2026-09-15 16:57:19 -0700

    Load Tuesday patches on Makers static pages

--- site.js
+++ site.js
@@ -4,4 +4,5 @@
  window.fmt=(n)=>Number(n).toLocaleString(undefined,{minimumFractionDigits:2,maximumFractionDigits:2});
  window.gradeClass=(g)=>{let x=(g||'').toLowerCase()[0];return ['a','b','c','d','f'].includes(x)?x:'b'};
  window.esc=(s)=>String(s??'').replace(/[&<>'"]/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;',"'":'&#39;','"':'&quot;'}[c]));
+ const data=document.createElement('script');data.src='makers-week1-tuesday-data.js?v=makers-20260915-tuesday1';data.onload=()=>{const patch=document.createElement('script');patch.src='makers-static-tuesday.js?v=makers-20260915-tuesday1';document.body.appendChild(patch)};document.body.appendChild(data);
 })();
